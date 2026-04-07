9101cd275dd212cb2cb4655e744abc747bf4bf4d
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-04-07 08:05:41 -0500

    Agregando test_10 registro producto

---
+++ src/main/java/com/ecommerce/service/ProductoService.java
@@ -0,0 +1,8 @@
+package com.ecommerce.service;
+
+public class ProductoService {
+    
+    public boolean registrarProducto(String nombre,float precio,int stock){
+        return true;
+    }
+}
---
+++ src/test/java/com/ecommerce/ProductoServiceTest.java
@@ -0,0 +1,28 @@
+package com.ecommerce;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.ecommerce.service.ProductoService;
+
+public class ProductoServiceTest {
+    private ProductoService productoService;
+
+    @BeforeEach
+    void setUp() {
+        productoService = new ProductoService();
+    }
+
+    @Test
+    void T10_agregarProducto(){
+        String nombre = "Laptop";
+        float precio =1200;
+        int stock =20;
+
+        boolean respuesta = productoService.registrarProducto(nombre,precio,stock);
+
+        assertTrue(respuesta,"El registro deberia de retornar true con los datos validos");
+    }
+    
+}
