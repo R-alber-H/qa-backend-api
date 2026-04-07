a63ac07ee25d4c9a35033f9fc01c62cbd7c56c21
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-04-07 17:20:20 -0500

    Agregando test_19 agregando producto no registrado

--- src/main/java/com/ecommerce/service/CarritoService.java
+++ src/main/java/com/ecommerce/service/CarritoService.java
@@ -6,8 +6,13 @@ import java.util.List;
 import com.ecommerce.model.Producto;
 
 public class CarritoService {
+    private ProductoService productoService;
     private List<Producto> productos = new ArrayList<>();
     private float total =0;
+
+    public CarritoService(ProductoService productoService) {
+        this.productoService = productoService; 
+    }
 
     public List<Producto> getProductos() {
         return productos;
@@ -18,6 +23,10 @@ public class CarritoService {
     }
 
     public boolean agregarProducto(Producto producto){
+        Producto productoEncontrado = productoService.buscarProducto(producto.getNombre());
+        if(productoEncontrado == null){
+            throw new IllegalArgumentException("Producto no registrado");
+        }
         productos.add(producto);
         this.total += producto.getPrecio();
         return true;
