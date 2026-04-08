2c0dc59d699dc7398a702c58d031a8f82274dfc3
Ricardo Alberco <sabado17_08_2024@outlook.com>
2026-04-08 08:04:53 -0500

    Agregando repositories

---
+++ src/main/java/com/ecommerce/Repository/ProductoRepository.java
@@ -0,0 +1,14 @@
+package com.ecommerce.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ecommerce.model.Producto;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long>{
+    
+    Optional<Producto> findByNombre(String nombre);
+}
